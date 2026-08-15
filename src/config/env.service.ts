1277bf09429338168827ddd63af0af2964eb6bc7
amirxnofal <amirxnofal.dev@gmail.com>
2026-08-15 21:16:26 +0300

    feat(env): add nodeEnv

--- src/config/env.service.ts
+++ src/config/env.service.ts
@@ -4,6 +4,7 @@ import path from "path";
 dotenv.config({ path: path.resolve(`./.env.${process.env.NODE_ENV}`) });
 
 const port = process.env.PORT;
+const nodeEnv = process.env.NODE_ENV;
 const mongodbUri = process.env.MONGODB_URI;
 const saltRound = process.env.SALT_ROUND;
 const googleAppEmail = process.env.GOOGLE_APP_EMAIL;
@@ -16,5 +17,6 @@ export const env = {
     saltRound,
     googleAppEmail,
     googleAppPassword,
-    emailSenderName
+    emailSenderName,
+    nodeEnv,
 };
