20af7431be370064eef849ba04b739e14ede9593
amirxnofal <amirxnofal.dev@gmail.com>
2026-08-15 23:13:04 +0300

    refactor(auth): update registerSchema

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -1,6 +1,27 @@
 import { z } from "zod";
+import { GenderEnum } from "../../common";
 
-export const loginSchema = z.object({
-    email: z.string().email("Invalid email format"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
-});
+export const registerSchema = z
+    .object({
+        username: z.string().trim().min(3).max(20),
+        firstName: z
+            .string()
+            .trim()
+            .min(3, "First Name must be at least 3 characters")
+            .max(50),
+        lastName: z
+            .string()
+            .trim()
+            .min(3, "Last Name must be at least 3 characters")
+            .max(50)
+            .optional(),
+        email: z.string().trim().toLowerCase().email("Invalid email format"),
+        phone: z.string().regex(/^01[0125][0-9]{8}$/, "Invalid phone number"),
+        password: z.string().min(8, "Password must be at least 8 characters"),
+        confirmPassword: z.string(),
+        gender: z.nativeEnum(GenderEnum),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "Passwords do not match",
+        path: ["confirmPassword"],
+    });
