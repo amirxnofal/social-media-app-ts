a51156edc167711b920dfd61a2af5a076ad85e3f
amirxnofal <amirxnofal.dev@gmail.com>
2026-08-15 23:13:29 +0300

    refactor(auth): update register route

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
+
 import authController from "./auth.controller";
 import { limiter } from "../../common/middlewares/rate-limit.middleware";
 import { Validation } from "../../common/middlewares/validation.middleware";
-import { loginSchema } from "./auth.validation";
+import * as v from "./auth.validation";
 
 const router = Router();
 
 router.post(
-    "/",
-    Validation(loginSchema),
-    limiter(10, 5, "Too many login attempts, please try again later"),
-    authController.getAllUsers,
+    "/register",
+    Validation(v.registerSchema),
+    limiter(10, 5, "Too many register attempts, please try again later"),
+    authController.register,
 );
+router.get("/rr", authController.returnAll);
 
 export default router;
