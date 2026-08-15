4de4b6765fc0ecc62f87f171ce58684b0f208694
amirxnofal <amirxnofal.dev@gmail.com>
2026-08-15 23:13:10 +0300

    feat(auth): integrate OTP generation and email sending in register flow

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,77 @@
+import {
+    BadRequestException,
+    ConflictException,
+    GenderEnum,
+    generateOtp,
+    hashText,
+    IUser,
+    otpEmailTemplate,
+    ProviderEnum,
+    RoleEnum,
+    sendEmail,
+} from "../../common";
 import userModel from "../../database/models/user.model";
+import * as RedisClient from "../../database/redis/redis.service";
 
 class AuthService {
-    async getAllUsers() {
-        const users = userModel.find();
+    async register(data: IUser) {
+        const {
+            username,
+            firstName,
+            lastName,
+            email,
+            phone,
+            password,
+            gender,
+        } = data;
+
+        const isEmailExist = await userModel.findOne({ email });
+        if (isEmailExist) throw new ConflictException("Email already exist");
+
+        const isUsernameExist = await userModel.findOne({ username });
+        if (isUsernameExist) throw new ConflictException("user already exist");
+
+        const isPhoneExist = await userModel.findOne({ phone });
+        if (isPhoneExist) throw new ConflictException("Phone already exist");
+
+        const hashedPassword = await hashText({ plainText: password });
+
+        const user = await userModel.create({
+            username,
+            firstName,
+            lastName,
+            email,
+            phone,
+            password: hashedPassword,
+            confirmEmail: false,
+            gender:
+                gender === GenderEnum.Male
+                    ? GenderEnum.Male
+                    : GenderEnum.Female,
+            provider: ProviderEnum.System,
+            role: RoleEnum.User,
+        });
+
+        const otp = generateOtp(6);
+        const hashedOtp = await hashText({ plainText: otp });
+
+        await RedisClient.set({
+            key: `otp:${user._id}`,
+            value: hashedOtp,
+            ttl: 5 * 60,
+        });
+
+        await sendEmail({
+            to: email,
+            subject: "OTP Verification",
+            html: otpEmailTemplate(otp),
+        });
+
+        return user;
+    }
+
+    async returnAll() {
+        const users = await userModel.find();
         return users;
     }
 }
