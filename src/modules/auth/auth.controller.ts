e53466ba4b6b113254d2185df65c32489e3d233a
amirxnofal <amirxnofal.dev@gmail.com>
2026-08-16 14:58:23 +0300

    feat(auth): add login and verifyEmail controllers, fix error handling

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,20 +1,60 @@
-import { type Request, type Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import authService from "./auth.service";
+import { SuccessResponse } from "../../common";
+import { loginDto, RegisterDto, verifyEmailDto } from "./auth.validation";
 
 class AuthController {
-    async register(req: Request, res: Response) {
-        const result = await authService.register(req.body);
-        res.status(200).json({
-            success: true,
-            message: "Successfull registeration",
-            data: result,
-        });
+    async register(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data: RegisterDto = req.body;
+            const result = await authService.register(data);
+            SuccessResponse({
+                res,
+                statusCode: 200,
+                message: "Successful registration",
+                data: result,
+            });
+        } catch (error) {
+            next(error);
+        }
     }
+
+    async login(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data: loginDto = req.body;
+
+            const result = await authService.login(data, req.get("host"));
+            SuccessResponse({
+                res,
+                statusCode: 200,
+                message: "Login success",
+                token: result,
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async verifyEmail(req: Request, res: Response, next: NextFunction) {
+        try {
+            const data: verifyEmailDto = req.body;
+
+            const result = await authService.verifyEmail(data);
+            SuccessResponse({
+                res,
+                statusCode: 200,
+                message: "Email verified",
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+
     async returnAll(req: Request, res: Response) {
         const result = await authService.returnAll();
         res.status(200).json({
             success: true,
-            message: "Successfull registeration",
+            message: "Done",
             data: result,
         });
     }
