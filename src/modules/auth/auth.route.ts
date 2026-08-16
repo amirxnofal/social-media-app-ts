1d3158c8213eaa6d636ae53b4f1ddcc274d30fbc
amirxnofal <amirxnofal.dev@gmail.com>
2026-08-16 14:58:32 +0300

    feat(auth): add login and verify-email routes

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -10,9 +10,24 @@ const router = Router();
 router.post(
     "/register",
     Validation(v.registerSchema),
-    limiter(10, 5, "Too many register attempts, please try again later"),
+    limiter(10, 3, "Too many register attempts, please try again later"),
     authController.register,
 );
+
+router.post(
+    "/login",
+    Validation(v.loginSchema),
+    limiter(10, 3, "Too many login attempts, please try again later"),
+    authController.login,
+);
+
+router.post(
+    "/verify",
+    Validation(v.verifyEmailSchema),
+    limiter(10, 3, "Too many attempts, please try again later"),
+    authController.verifyEmail,
+);
+
 router.get("/rr", authController.returnAll);
 
 export default router;
